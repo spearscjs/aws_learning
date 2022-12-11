42b68b048578bd9523e7934f80a12c26c5c8cfdc
demlsp2040 <demlsp2040@gmail.com>
2022-12-11 08:49:12 -0500

    added Sql table,data insert and question for practise

---
+++ sql_learing/questions/group1/question2.sql
@@ -0,0 +1,8 @@
+1.SQL Query to print the number of employees per department in the organization
+2.SQL Query to find the employee details who got second maximum salary
+3.SQL Query to find the employee details who got second maximum salary in each department
+4.SQL Query to find the employee who got minimum salary in 2019
+5.SQL query to select the employees getting salary greater than the average salary of the department that are working in
+6.SQL query to compute the group salary of all the employees.
+7.SQL query to list the employees and name of employees reporting to each person.
+8.SQL query to find the department with highest number of employees.
