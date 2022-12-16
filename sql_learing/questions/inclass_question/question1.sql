69849ab0c026ab8cf2549fae0b21f0d38a902234
demlsp2040 <demlsp2040@gmail.com>
2022-12-16 08:33:23 -0500

    addedd dim data and also question for join

--- sql_learing/questions/inclass_question/question1.sql
+++ sql_learing/questions/inclass_question/question1.sql
@@ -40,14 +40,15 @@ truncate table cards_ingest.cust_dim_details;
 insert into cards_ingest.cust_dim_details
 (cust_id,state_cd,zip_cd , cust_first_name, cust_last_name, start_date,end_date,active_flag)
 VALUES
-('cust_101','NY','08922', 'Mike', 'doge',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyy-mm-dd'),'Y'),
-('cust_102','CA','04922', 'sean', 'lan',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyy-mm-dd'),'Y'),
-('cust_103','CA','05922', 'sachin', 'ram',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyy-mm-dd'),'Y'),
-('cust_104','TX','08942', 'bill', 'kja',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyy-mm-dd'),'Y'),
-('cust_105','CA','08122', 'Douge', 'lilly',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyy-mm-dd'),'Y'),
-('cust_106','CA','08322', 'hence', 'crow',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyy-mm-dd'),'Y'),
-('cust_107','TX','08722', 'Mike', 'doge',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-02-01','yyy-mm-dd'),'Y'),
-('cust_107','NY','02122', 'Mike', 'doge',to_date('2022-02-03','yyyy-mm-dd'),to_date('2022-01-01','yyy-mm-dd'),'N');
+('cust_101','NY','08922', 'Mike', 'doge',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyyy-mm-dd'),'Y'),
+('cust_102','CA','04922', 'sean', 'lan',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyyy-mm-dd'),'Y'),
+('cust_103','CA','05922', 'sachin', 'ram',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyyy-mm-dd'),'Y'),
+('cust_104','TX','08942', 'bill', 'kja',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyyy-mm-dd'),'Y'),
+('cust_105','CA','08122', 'Douge', 'lilly',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyyy-mm-dd'),'Y'),
+('cust_106','CA','08322', 'hence', 'crow',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-01-01','yyyy-mm-dd'),'Y'),
+('cust_107','TX','08722', 'Mike', 'doge',to_date('2022-01-01','yyyy-mm-dd'),to_date('2029-02-01','yyyy-mm-dd'),'Y'),
+('cust_107','NY','02122', 'Mike', 'doge',to_date('2022-02-03','yyyy-mm-dd'),to_date('2022-01-01','yyyy-mm-dd'),'N');
+
 
 
 1. Calculate total tran_ammt (sum) for each state
