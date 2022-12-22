10abbaea29db3b4460fd34b3476cb0e0c05df90b
demlsp2040 <demlsp2040@gmail.com>
2022-12-22 08:12:10 -0500

    sql class practise question

--- sql_learing/questions/inclass_question/question2.sql
+++ sql_learing/questions/inclass_question/question2.sql
@@ -11,13 +11,13 @@ INSERT INTO cards_ingest.tran_fact
 	(tran_id, cust_id, stat_cd, tran_ammt, tran_date) VALUES
 	(102020, 'cust_101', 'NY', 125,to_date('2022-01-01','yyyy-mm-dd')),
 	(102021, 'cust_101', 'NY', 5125,to_date('2022-01-01','yyyy-mm-dd')),
-	(102021, 'cust_101', 'NY', 225,to_date('2022-02-01','yyyy-mm-dd')),
-	(102021, 'cust_101', 'NY', 4125,to_date('2022-02-03','yyyy-mm-dd')),
-    (102022, 'cust_102', 'CA', 6125,to_date('2022-01-01','yyyy-mm-dd')),
-    (102023, 'cust_103', 'CA', 7145,to_date('2022-01-01','yyyy-mm-dd')),
-    (102023, 'cust_103', 'CA', 7145,to_date('2022-04-01','yyyy-mm-dd')),
-    (102023, 'cust_103', 'CA', 7145,to_date('2022-03-01','yyyy-mm-dd')),
-    (102023, 'cust_103', 'CA', 7145,to_date('2022-03-02','yyyy-mm-dd')),
+	(1020321, 'cust_101', 'NY', 225,to_date('2022-02-01','yyyy-mm-dd')),
+	(1020121, 'cust_101', 'NY', 4125,to_date('2022-02-03','yyyy-mm-dd')),
+    (1020222, 'cust_102', 'CA', 6125,to_date('2022-01-01','yyyy-mm-dd')),
+    (1020223, 'cust_103', 'CA', 7145,to_date('2022-01-01','yyyy-mm-dd')),
+    (1023023, 'cust_103', 'CA', 7145,to_date('2022-04-01','yyyy-mm-dd')),
+    (1020123, 'cust_103', 'CA', 7145,to_date('2022-03-01','yyyy-mm-dd')),
+    (1020223, 'cust_103', 'CA', 7145,to_date('2022-03-02','yyyy-mm-dd')),
     (102024, 'cust_104', 'TX', 1023,to_date('2022-01-01','yyyy-mm-dd')),
     (102025, 'cust_101', 'NY', 670,to_date('2022-01-03','yyyy-mm-dd')),
 	(102026, 'cust_101', 'NY', 5235,to_date('2022-01-03','yyyy-mm-dd')),
@@ -27,9 +27,9 @@ INSERT INTO cards_ingest.tran_fact
     (102030, 'cust_109', NULL, 1023,to_date('2022-01-05','yyyy-mm-dd')),
     (102031, 'cust_104',Null, 1023,to_date('2022-01-05','yyyy-mm-dd')),
     (102031, 'cust_107','TX', 4000,to_date('2022-01-05','yyyy-mm-dd')),
-    (102031, 'cust_107','TX', 4000,to_date('2022-02-05','yyyy-mm-dd')),
-    (102031, 'cust_107','TX', 4000,to_date('2022-02-03','yyyy-mm-dd')),
-    (102031, 'cust_107','CA', 7000,to_date('2022-02-05','yyyy-mm-dd'));
+    (1022031, 'cust_107','TX', 4000,to_date('2022-02-05','yyyy-mm-dd')),
+    (10202231, 'cust_107','TX', 4000,to_date('2022-02-03','yyyy-mm-dd')),
+    (1302031, 'cust_107','CA', 7000,to_date('2022-02-05','yyyy-mm-dd'));
 
 drop table cards_ingest.cust_dim_details;
 create table cards_ingest.cust_dim_details (
